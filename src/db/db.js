31a091949abf4c1d50178444192c1b805588ac1f
Seth Mwebi <sethmwebi27@gmail.com>
2026-09-21 14:11:09 +0300

    implement matches POST and GET routes

--- src/db/db.js
+++ src/db/db.js
@@ -6,7 +6,8 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 export const db = drizzle(pool);
-
