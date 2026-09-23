6baad614e13d2c07ebc16caf0389b355b80eabe0
Seth Mwebi <sethmwebi27@gmail.com>
2026-09-23 07:56:39 +0300

    implement coderabbit suggested fixes

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -18,7 +18,7 @@ matchRouter.get("/", async (req, res) => {
   if (!parsed.success) {
     return res.status(400).json({
       error: "Invalid query.",
-      details: JSON.stringify(parsed.error),
+      details: parsed.error.issues,
     });
   }
 
@@ -69,7 +69,7 @@ matchRouter.post("/", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       error: "Failed to create match.",
-      details: JSON.stringify(error.message),
+      details: parsed.error.issues,
     });
   }
 });
